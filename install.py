d39b69b6c94800fccd4f7b31b1edb7017377c916
tobiblas <tobias2.jonsson@gmail.com>
2016-01-24 21:45:58 +0100

    Changes to install script. Handling non admin PIs.

--- install.py
+++ install.py
@@ -1,47 +1,66 @@
 import os
 import subprocess
 
+######### CHECK IF THIS PI SHOULD HAVE SERVER ###############
+isServer = False
+inputCorrect = False
+while inputCorrect == False:
+    mainBrain = raw_input("Should this Raspberry Pi run a php server i.e is this Pi the main brain of the alarm system? (Y/n). ")
+    if mainBrain == 'Y' or mainBrain == 'y' or mainBrain == '':
+        print "you have chosen to use this Pi as main brain in the alarm system."
+        isServer = True
+        inputCorrect = True
+    elif mainBrain == 'N' or mainBrain == 'n':
+        print "you have chosen NOT to use this Pi as main brain in the alarm system."
+        inputCorrect = True
+    else:
+        print "Please enter valid input 'y' or 'n'."
+
 ################# APACHE2 ####################################
 
-print "Checking for apache installation..."
+if isServer:
+    print "Checking for apache installation..."
 
-apache2path = subprocess.Popen("which apache2", shell=True, stdout=subprocess.PIPE).stdout.read()
+    apache2path = subprocess.Popen("which apache2", shell=True, stdout=subprocess.PIPE).stdout.read()
 
-if "apache2" not in apache2path:
-	print "installing apache web server."
-	print subprocess.Popen("sudo apt-get install apache2 -y", shell=True, stdout=subprocess.PIPE).stdout.read()
-else:
-	print "apache already installed. Skipping"
+    if "apache2" not in apache2path:
+        print "installing apache web server."
+        print subprocess.Popen("sudo apt-get install apache2 -y", shell=True, stdout=subprocess.PIPE).stdout.read()
+    else:
+        print "apache already installed. Skipping"
 
-print "-----------"
+    print "-----------"
 
 ################# PHP ########################################
 
-print "Checking for php installation..."
+if isServer:
+    print "Checking for php installation..."
 
-phpPath = subprocess.Popen("which php", shell=True, stdout=subprocess.PIPE).stdout.read()
+    phpPath = subprocess.Popen("which php", shell=True, stdout=subprocess.PIPE).stdout.read()
 
-if "php" not in phpPath:
-	print "installing php."
-	print subprocess.Popen("sudo apt-get install php5 libapache2-mod-php5 -y", shell=True, stdout=subprocess.PIPE).stdout.read()
-else:
-	print "php already installed. Skipping"
+    if "php" not in phpPath:
+        print "installing php."
+        print subprocess.Popen("sudo apt-get install php5 libapache2-mod-php5 -y", shell=True, stdout=subprocess.PIPE).stdout.read()
+    else:
+        print "php already installed. Skipping"
 
-print "-----------"
+    print "-----------"
 
 ################# MOVE PHP PAGE TO RIGHT PLACE ###############
 
-print "adding php admin page for alarm."
-print "installing php pages in /var/www/html/"
+if isServer:
+    print "adding php admin page for alarm."
+    print "installing php pages in /var/www/html/"
 
-print subprocess.Popen("sudo mkdir -p /var/www/html/alarm && sudo cp -R php/* /var/www/html/alarm", shell=True, stdout=subprocess.PIPE).stdout.read()
+    print subprocess.Popen("sudo mkdir -p /var/www/html/alarm && sudo cp -R php/* /var/www/html/alarm", shell=True, stdout=subprocess.PIPE).stdout.read()
 
-print "-----------"
+    print "-----------"
+
 #################ALARM FILES, CONFIG ETC######################
 pathCorrect = False
 alarmPath = ""
 while pathCorrect == False:
-	alarmPath = raw_input("Enter path to alarm configuration ")
+	alarmPath = raw_input("Enter path to alarm home folder ")
 
 	if os.path.exists(alarmPath):
 		print alarmPath + " exists." 
@@ -61,14 +80,25 @@ if not alarmPath.endswith("/"):
 	alarmPath += "/"
 print subprocess.Popen("cp -R alarm " + alarmPath, shell=True, stdout=subprocess.PIPE).stdout.read()
 
-################# SETTING UP CONFIGURATION ##################
-
-print "setting up configuration for alarm."
-
-#TODO enter input to the properties file.
-
+if isServer:
+    print "Adding alarm home to admin.properties"
+    print subprocess.Popen('echo "alarm_home:' + alarmPath + '" | sudo tee /var/www/html/alarm/admin.properties', shell=True, stdout=subprocess.PIPE).stdout.read()
 ################## PHONE ####################################
 
 #Ask user if attaching phone to pi
+phoneConnected = False
+inputCorrect = False
+while inputCorrect == False:
+    phone = raw_input("Should this Raspberry Pi use an Android Phone as part of the system (sending alarm sms, taking pictures)? (Y/n). ")
+    if phone == 'Y' or phone == 'y' or phone == '':
+        print "you have chosen to have a phone connected to this Pi."
+        phoneConnected = True
+        inputCorrect = True
+    elif phone == 'N' or phone == 'n':
+        print "you have chosen NOT to have a phone connected to this Pi."
+        inputCorrect = True
+    else:
+        print "Please enter valid input 'y' or 'n'."
 
-#sudo apt-get install adb-tools-android
+if phoneConnected:
+    print subprocess.Popen('sudo apt-get install adb-tools-android', shell=True, stdout=subprocess.PIPE).stdout.read()
