dc7e6216295aafe1a095559028bff6869f8553aa
tobiblas <tobias2.jonsson@gmail.com>
2017-01-05 15:39:08 +0100

    fixed crone copy

--- alarm/copycron.sh
+++ alarm/copycron.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
- 
-if [ $(cat /var/www/html/alarm/crontab.txt | wc -l) -gt "0" ]; then 
-	crontab -r -u pi; 
-	crontab -u pi /var/www/html/alarm/crontab.txt && > /var/www/html/alarm/crontab.txt; 
-fi
 
+# Input file
+FILE=/var/www/html/alarm/crontab.txt
+CHANGETIME=300
+# Get current and file times
+CURTIME=$(date +%s)
+FILETIME=$(stat $FILE -c %Y)
+TIMEDIFF=$(expr $CURTIME - $FILETIME)
+
+# Check if file older
+if [ $TIMEDIFF -lt $CHANGETIME ]; then
+echo "File changed last minutes"
+crontab -r -u pi;
+crontab -u pi /var/www/html/alarm/crontab.txt
+fi
