12c081341b4da895141c550a5b8acb12ae158878
tobiblas <tobias2.jonsson@gmail.com>
2017-01-05 14:58:50 +0100

    ON and OFF in cron

--- php/add_to_cron.php
+++ php/add_to_cron.php
@@ -1,5 +1,8 @@
 <?php
     $value = $_GET['value'];
+    
+    $value = str_replace("ON", "wget -qO- localhost/alarm/toggle_alarm.php?enable=true &> /dev/null", $value);
+    $value = str_replace("OFF", "wget -qO- localhost/alarm/toggle_alarm.php?enable=false &> /dev/null", $value);
 
     $command = 'echo "' . $value .'" > /var/www/html/alarm/crontab.txt';
     echo exec($command . ' 2>&1');
--- php/getCron.php
+++ php/getCron.php
@@ -3,6 +3,8 @@
     $command = 'cat crontab.txt';
     exec($command . ' 2>&1', $out);
     foreach($out as $line) {
+        $line = str_replace("wget -qO- localhost/alarm/toggle_alarm.php?enable=true &> /dev/null", "ON", $line);
+        $line = str_replace("wget -qO- localhost/alarm/toggle_alarm.php?enable=false &> /dev/null", "OFF", $line);
         echo $line . PHP_EOL;
     }
 ?>
