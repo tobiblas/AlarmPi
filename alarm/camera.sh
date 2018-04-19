a951673867835fbad14c272cdc6310acb7ffade2
tobiblas <tobias2.jonsson@gmail.com>
2018-04-19 23:55:48 +0200

    4 pics for the camera.

--- alarm/camera.sh
+++ alarm/camera.sh
@@ -2,5 +2,4 @@
 
 DATE=$(date +"%Y-%m-%d_%H%M%S")
 
-raspistill -t 2500 -vf -hf -w 648 -h 486 -q 50 -o /var/www/html/photos/$DATE.jpg
-
+raspistill -vf -hf -w 648 -h 486 -q 50 -o /var/www/html/photos/2018_0%d.jpg -tl 1000 -t 3000 -dt
